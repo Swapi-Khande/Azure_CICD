157488724ac845a0e0d64c832f0608163915dd12
Swapi-Khande <swapnilkhande6996@gmail.com>
2026-08-01 20:54:08 +0530

    Comment added

--- TestWebApp/Controllers/HomeController.cs
+++ TestWebApp/Controllers/HomeController.cs
@@ -26,6 +26,7 @@ namespace TestWebApp.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            // Error View
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
