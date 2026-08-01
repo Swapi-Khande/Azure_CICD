ff8abb23fd3dc93849f228faf9625610cc067a68
Swapi-Khande <swapnilkhande6996@gmail.com>
2026-08-01 23:08:01 +0530

    new commit

--- TestWebApp/Controllers/HomeController.cs
+++ TestWebApp/Controllers/HomeController.cs
@@ -26,7 +26,7 @@ namespace TestWebApp.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            // Error View - Default 2
+            // Error View - Default 20
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
